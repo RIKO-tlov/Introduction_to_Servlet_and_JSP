8140454934d60723af978ba08961f3dfc1a42605
RIKO-tlov <bluebyrair@outlook.jp>
2021-08-23 23:34:52 +0900

    [start]Chapter3 servlet class

---
+++ Chapter3/HelloServlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.httpServletRequest;
+import javax.servlet.httpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+  protected void doGet(httpServletRequest request,
+      httpServletResponse response)
+      throws ServletException, IOException {
+  }
+}
